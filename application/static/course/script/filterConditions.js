6177f289a6223667a35bc09aeac2b3ff4eb272af
XF <xf@songyudeMacBook-Air.local>
2016-06-25 22:18:56 +0800

    filterConditions

---
+++ application/static/course/script/filterConditions.js
@@ -0,0 +1,37 @@
+var filterConditions = function() {
+
+    var url = '#';
+
+    var runFilter = function() {
+        $('#filter').click(function(){
+            var params = {
+                currentPage : $('#currentPage').val(),
+                currentTeacher : $('#currentTeacher').val(),
+                currentSemester : $('#currentSemester').val(),
+                currentDepartment : $('#currentDepartment').val(),
+                currentSyllabusYear : $('#currentSyllabusYear').val()
+            };
+
+            var paramsTplArray = [
+                '?',
+                'currentPage={currentPage}',
+                '&currentTeacher={currentTeacher}',
+                '&currentSemester={currentSemester}',
+                '&currentDepartment={currentDepartment}',
+                '&currentSyllabusYear={currentSyllabusYear}'
+            ];
+            var paramsTpl = paramsTplArray.join('');
+            window.location.href = url + format(paramsTpl, params);
+        });
+    };
+
+    return {
+        setActionUrl: function(u){
+          url = u;
+        },
+        init: function() {
+            runFilter();
+        }
+    };
+
+}();
