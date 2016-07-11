36f55db695c6d5b1839fe030d0fdef19251037f3
xf <arlequin@163.com>
2016-07-11 21:52:32 +0800

    random string

--- application/utilities.py
+++ application/utilities.py
@@ -36,7 +36,7 @@ def role_required(role):
     return decorator
 
 
-def generate_random_string(length=8, characters=string.ascii_lowercase+string.digits):
+def generate_random_string(length=16, characters=string.ascii_letters+string.digits):
     return ''.join(random.choice(characters) for _ in range(length))
 
 
